af36c6af94e35d5cf5f763228d42d0c6a6bbf79c
rasmuslundmark <rasmuslbus@gmail.com>
2023-05-04 14:41:38 +0200

    uppdatera

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -1,13 +1,39 @@
 package org.example;
 
+import com.codeborne.selenide.ex.ElementNotFound;
+
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
 
 public class Test {
 
+
+
 public static void finalExam(){
     open("https://www.ltu.se");
-    $("#search").setValue("selenium").pressEnter();
+    try{
+        if(title().isEmpty()){
+            System.out.println("Title is empty");
+        }
+        else {
+            System.out.println("Page loaded");
+        }
+    } catch(Exception e) {
+        System.out.println("Failed to load Page: " + e.getMessage());
+
+    }
+ try{
+     if($("button.CybotCookiebotDialogBodyButton").isDisplayed()){
+         $("button.CybotCookiebotDialogBodyButton").click();
+         System.out.println("Cookie accepted");
+     }
+
+ } catch(ElementNotFound e) {
+     System.out.println("Element not found");
+
+    }
+    $("button.CybotCookiebotDialogBodyButton").click();
+     
 }
 
 
