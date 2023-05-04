bb4d9710c9a10c6c213ec91eaf4a59f24810b648
Daniel <danielhammar00@gmail.com>
2023-05-04 15:31:25 +0200

    Jag har ändrat så koden FUNKAR

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -1,8 +1,13 @@
 package org.example;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.conditions.Visible;
 import com.codeborne.selenide.ex.ElementNotFound;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 import java.io.File;
 
@@ -13,6 +18,10 @@ public class Test {
 
 private static String email;
 private static String password;
+
+
+
+
 public static void readFile(){
     try {
         File jsonFile = new File("C:\\temp\\Login.json");
@@ -50,15 +59,44 @@ public static void finalExam(){
 
     }
     $("button.CybotCookiebotDialogBodyButton").click();
-     
+
+
+    // Student knapp
+    $("html > body > header > div:nth-of-type(2) > div:nth-of-type(1) > div:nth-of-type(1) > div:nth-of-type(3) > div > a:nth-of-type(1)").click();
+
+    // Registerutdrag
+    $("html > body > main > div > div > div:nth-of-type(1) > div > div:nth-of-type(2) > div > div > div > div > ul > li:nth-of-type(1) > a > div").click();
+
+    // Välj lärosäte
+    $("a[class$='btn-ladok-inloggning']").click();
+
+    // Prompt
+    $("input[id='searchinput']").setValue("LTU").click();
+    
+    // TRÖCK
+    $("li").shouldBe(visible).click();
+
+
+    // Användarnamn
+    $("input[id='username']").shouldBe(visible).setValue(email);
+    $("input[id='password']").setValue(password);
+
+    // Click
+    $("input[class='btn-submit']").click();
+
+
 }
 
 
+public static void loop(){
+    while (true);
+}
 
 
     public static void main(String[] args) {
         readFile();
         finalExam();
+        loop();
     }
 
 }
